35ec52b902a9de26db185253a2e9fcc3f03b262e
taraskohut <tarasikkog@gmail.com>
2023-02-01 13:44:35 +0200

    task1

---
+++ js_lessons/maine.js
@@ -0,0 +1,53 @@
+let hel = 'hello'
+console.log(hel)
+
+let ow = 'owu'
+console.log(ow)
+
+let co = 'com'
+console.log(co)
+
+let u = 'ua'
+console.log(u)
+
+let first = 1
+console.log(first)
+
+let second = 10
+console.log(second)
+
+let thr = 999
+console.log(thr)
+
+let ott = 123
+console.log(ott)
+
+let p = 3.14
+console.log(p)
+
+let ts = 2.7;
+console.log(ts)
+
+let os = 16;
+console.log(os)
+
+let tr = true;
+console.log(tr)
+
+let fl = false;
+console.log(fl)
+
+let firstName = 'Kohut'
+let middleName = 'Taras'
+let lastName = 'Bogdanovich'
+let person = firstName + middleName + lastName
+console.log(person)
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true
+console.log(typeof c)
+
