10c323f5078c237d8e0d00907c34e6e42c17dc4d
taraskohut <tarasikkog@gmail.com>
2023-02-01 14:04:42 +0200

    task1

--- js_lessons/maine.js
+++ js_lessons/maine.js
@@ -1,3 +1,5 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
 let hel = 'hello'
 console.log(hel)
 
@@ -22,8 +24,8 @@ console.log(thr)
 let ott = 123
 console.log(ott)
 
-let p = 3.14
-console.log(p)
+let P = 3.14
+console.log(P)
 
 let ts = 2.7;
 console.log(ts)
@@ -37,6 +39,7 @@ console.log(tr)
 let fl = false;
 console.log(fl)
 
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 let firstName = 'Kohut'
 let middleName = 'Taras'
 let lastName = 'Bogdanovich'
@@ -44,10 +47,16 @@ let person = firstName + middleName + lastName
 console.log(person)
 console.log(firstName + ' ' + middleName + ' ' + lastName)
 
+
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
 let a = 100;
 console.log(typeof a)
 let b = '100';
 console.log(typeof b)
 let c = true
 console.log(typeof c)
+
+
+
 
